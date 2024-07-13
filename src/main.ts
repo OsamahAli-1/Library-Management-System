82715d5162b5d68ebf517feb1b048a5ee2a834dc
OsamahAli-1 <samde9060@gmail.com>
2024-07-13 14:37:41 +0300

    Configuring database connection

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
+
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('Library Management System')
     .setDescription('Library Management System API description')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('doc', app, document);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('PORT', 3000);
+  await app.listen(port);
 }
 bootstrap();
